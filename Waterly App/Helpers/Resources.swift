8f38696372ee59e440816fbb1912623f6c449c29
Oleg Fedotov <trevor706@gmail.com>
2022-12-26 19:22:51 +0300

    Create custom TabBar

---
+++ Waterly App/Helpers/Resources.swift
@@ -0,0 +1,71 @@
+//
+//  Resources.swift
+//  Waterly App
+//
+//  Created by Olegio on 26.12.2022.
+//
+
+import UIKit
+
+enum Resources {
+    
+    enum Colors {
+        
+        enum Accent {
+            static let accentMain = UIColor(named: "AccentMain")
+            static let accentInactive = UIColor(named: "AccentInactive")
+        }
+        
+        enum Back {
+            static let backMain = UIColor(named: "BackMain")
+            static let backSecondary = UIColor(named: "BackSecondary")
+            static let textFieldBack = UIColor(named: "TextFieldBack")
+            static let tabBarBackColor = UIColor(named: "TabBarBackColor")
+        }
+        
+        enum Separator {
+            static let separatorAccent = UIColor(named: "SeparatorAccent")
+            static let separatorAccentSecondary = UIColor(named: "SeparatorAccentSecondary")
+        }
+        
+        enum Text {
+            static let textMain = UIColor(named: "TextMain")
+            static let textSecondary = UIColor(named: "TextSecondary")
+            static let textTertiary = UIColor(named: "TextTertiary")
+        }
+        
+        enum Other {
+            static let green = UIColor(named: "Green")
+            static let red = UIColor(named: "Red")
+            static let yellow = UIColor(named: "Yellow")
+            static let shadow = UIColor(named: "Shadow")
+        }
+    }
+    
+    enum Shadows {
+        
+        enum TabBar {
+            static let offset = CGSize(width: 0, height: -4)
+            static let opacity: Float = 0.5
+            static let radius: CGFloat = 22
+        }
+    }
+    
+    enum Strings {
+        
+        enum TabBar {
+            static let statistics = "Статистика"
+            static let main = "Главная"
+            static let settings = "Настройки"
+        }
+    }
+    
+    enum Images {
+        
+        enum TabBar {
+            static let statistics = UIImage(named: "statistics")
+            static let main = UIImage(named: "main")
+            static let settings = UIImage(named: "settings")
+        }
+    }
+}
---
+++ Waterly App/TabBarController.swift
@@ -0,0 +1,120 @@
+//
+//  TabBarController.swift
+//  Waterly App
+//
+//  Created by Olegio on 26.12.2022.
+//
+
+import UIKit
+
+enum Tabs: Int {
+    case statistics
+    case main
+    case settings
+}
+
+final class TabBarController: UITabBarController {
+    
+    private var customTabBarView = UIView(frame: .zero)
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        configureTabs()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabBarUI()
+        addCustomTabBarView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        setupCustomTabBarFrame()
+    }
+    
+    private func setupCustomTabBarFrame() {
+        let height = view.safeAreaInsets.bottom + 56
+        
+        var tabFrame = tabBar.frame
+        tabFrame.size.height = height
+        tabFrame.origin.y = view.frame.size.height - height
+        
+        tabBar.frame = tabFrame
+        tabBar.setNeedsLayout()
+        tabBar.layoutIfNeeded()
+        
+        customTabBarView.frame = tabBar.frame
+    }
+    
+    private func setupTabBarUI() {
+        tabBar.backgroundColor = Resources.Colors.Back.tabBarBackColor
+        tabBar.layer.cornerRadius = 30
+        tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        tabBar.tintColor = Resources.Colors.Accent.accentMain
+        
+        if #available(iOS 13.0, *) {
+            let appearence = tabBar.standardAppearance
+            appearence.stackedLayoutAppearance.normal.titleTextAttributes =
+            [NSAttributedString.Key.foregroundColor: Resources.Colors.Accent.accentInactive as Any]
+            appearence.stackedLayoutAppearance.normal.iconColor = Resources.Colors.Accent.accentInactive
+            appearence.shadowImage = nil
+            appearence.shadowColor = nil
+            tabBar.standardAppearance = appearence
+        } else {
+            tabBar.shadowImage = UIImage()
+            tabBar.backgroundImage = UIImage()
+        }
+    }
+    
+    private func addCustomTabBarView() {
+        customTabBarView.frame = tabBar.frame
+        
+        customTabBarView.backgroundColor = Resources.Colors.Back.tabBarBackColor
+        customTabBarView.layer.cornerRadius = 30
+        customTabBarView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        customTabBarView.layer.masksToBounds = false
+        customTabBarView.layer.shadowColor =
+            Resources.Colors.Other.shadow?.withAlphaComponent(0.1).cgColor
+        customTabBarView.layer.shadowOffset = Resources.Shadows.TabBar.offset
+        customTabBarView.layer.shadowOpacity = Resources.Shadows.TabBar.opacity
+        customTabBarView.layer.shadowRadius = Resources.Shadows.TabBar.radius
+        
+        view.addSubview(customTabBarView)
+        view.bringSubviewToFront(tabBar)
+    }
+    
+    private func configureTabs() {
+        let statisticsController = UIViewController()
+        let mainController = UIViewController()
+        let settingsController = UIViewController()
+        
+        let settingsNavController = UINavigationController(rootViewController: settingsController)
+        
+        statisticsController.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.statistics,
+                                                       image: Resources.Images.TabBar.statistics,
+                                                       tag: Tabs.statistics.rawValue)
+        
+        mainController.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.main,
+                                                 image: Resources.Images.TabBar.main,
+                                                 tag: Tabs.main.rawValue)
+        
+        settingsNavController.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.settings,
+                                                        image: Resources.Images.TabBar.settings,
+                                                        tag: Tabs.settings.rawValue)
+        
+        setViewControllers([
+            statisticsController,
+            mainController,
+            settingsNavController
+        ], animated: false)
+    }
+}
