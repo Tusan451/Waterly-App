68d9a0a26ec027da2fe90769d59055c9309cb34f
Oleg Fedotov <trevor706@gmail.com>
2022-12-26 14:58:50 +0300

    Initial Commit

---
+++ Waterly App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Waterly App
+//
+//  Created by Olegio on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
