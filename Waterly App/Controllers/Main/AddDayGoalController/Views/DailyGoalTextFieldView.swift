81bad6e8c56d574568a898613c226cd5741badcb
Oleg Fedotov <trevor706@gmail.com>
2023-01-13 19:15:44 +0300

    add uiLabel, add uiTextField

--- Waterly App/Controllers/Main/AddDayGoalController/Views/DailyGoalTextFieldView.swift
+++ Waterly App/Controllers/Main/AddDayGoalController/Views/DailyGoalTextFieldView.swift
@@ -5,4 +5,55 @@
 //  Created by Olegio on 13.01.2023.
 //
 
-import Foundation
+import UIKit
+
+final class DailyGoalTextFieldView: BaseView {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.sfProRegular(size: 14)
+        label.textColor = Resources.Colors.Text.textSecondary
+        label.text = Resources.Strings.MainController.AddDayGoalController.currentGoal
+        return label
+    }()
+    
+    private let textField = BaseTextField(width: UIScreen.main.bounds.width - 40, height: 50)
+    
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 12
+        return view
+    }()
+    
+    func setTextFieldValue(text: String) {
+        textField.setText(text)
+    }
+}
+
+extension DailyGoalTextFieldView {
+    
+    override func addViews() {
+        super.addViews()
+        
+        addView(stackView)
+        
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(textField)
+    }
+    
+    override func layoutViews() {
+        super.layoutViews()
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+    }
+}
