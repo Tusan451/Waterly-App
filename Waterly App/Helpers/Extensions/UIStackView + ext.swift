d55ee48672f5dc52d2202fca72f50a633450a4aa
Oleg Fedotov <trevor706@gmail.com>
2023-01-17 21:08:05 +0300

    Modify UIStatckView + ext.swift

--- Waterly App/Helpers/Extensions/UIStackView + ext.swift
+++ Waterly App/Helpers/Extensions/UIStackView + ext.swift
@@ -5,4 +5,21 @@
 //  Created by Olegio on 17.01.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+    
+    func removeAllArrangedSubviews() {
+        
+        let removedSubviews = arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
+            self.removeArrangedSubview(subview)
+            return allSubviews + [subview]
+        }
+        
+        // Deactivate all constraints
+        NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
+        
+        // Remove the views from self
+        removedSubviews.forEach({ $0.removeFromSuperview() })
+    }
+}
