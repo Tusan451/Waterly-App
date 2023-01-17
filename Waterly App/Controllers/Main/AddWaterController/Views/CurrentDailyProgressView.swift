6f528eb03d8c522b8e91da95ef578867316f13c9
Oleg Fedotov <trevor706@gmail.com>
2023-01-17 19:40:11 +0300

    Update CurrentDailyProgressView.swift
    add UI elements
    add logic for calculate current percent progress
    add logic for calculate new percent progress

--- Waterly App/Controllers/Main/AddWaterController/Views/CurrentDailyProgressView.swift
+++ Waterly App/Controllers/Main/AddWaterController/Views/CurrentDailyProgressView.swift
@@ -5,4 +5,91 @@
 //  Created by Olegio on 17.01.2023.
 //
 
-import Foundation
+import UIKit
+
+final class CurrentDailyProgressView: BaseView {
+    
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.sfProMedium(size: 13)
+        label.textColor = Resources.Colors.Text.textSecondary
+        label.text = Resources.Strings.MainController.AddWaterController.currentProgress
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let currentValueLabel: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.sfProSemibold(size: 24)
+        label.textColor = Resources.Colors.Text.textMain
+        return label
+    }()
+    
+    private let arrowView: UIImageView = {
+        let view = UIImageView()
+        view.image = Resources.Images.AddWaterViewController.arrow?.withRenderingMode(.alwaysTemplate)
+        view.tintColor = Resources.Colors.Text.textMain
+        view.isHidden = true
+        return view
+    }()
+    
+    private let newValueLabel: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.sfProSemibold(size: 24)
+        label.textColor = Resources.Colors.Accent.accentMain
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.spacing = 7
+        view.distribution = .equalSpacing
+        return view
+    }()
+    
+    func configureValueLabels(current: String, new: String? = nil) {
+        currentValueLabel.text = current
+        
+        if let newValue = new {
+            newValueLabel.text = newValue
+            arrowView.isHidden = false
+        }
+    }
+}
+
+extension CurrentDailyProgressView {
+    
+    override func addViews() {
+        super.addViews()
+        
+        addView(textLabel)
+        addView(stackView)
+        
+        stackView.addArrangedSubview(currentValueLabel)
+        stackView.addArrangedSubview(arrowView)
+        stackView.addArrangedSubview(newValueLabel)
+    }
+    
+    override func layoutViews() {
+        super.layoutViews()
+        
+        NSLayoutConstraint.activate([
+            textLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textLabel.topAnchor.constraint(equalTo: topAnchor),
+            textLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            textLabel.bottomAnchor.constraint(equalTo: stackView.topAnchor, constant: -14),
+            
+            arrowView.widthAnchor.constraint(equalToConstant: 24),
+            arrowView.heightAnchor.constraint(equalToConstant: 24),
+            
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 14),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+    }
+}
