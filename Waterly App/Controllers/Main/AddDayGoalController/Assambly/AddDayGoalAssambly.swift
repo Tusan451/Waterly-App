c0523ae3531de343202f9c3368153090792c789c
Oleg Fedotov <trevor706@gmail.com>
2023-01-21 18:22:47 +0300

    Manage file structure
    new:
    AddDayGoalAssambly.swift
    AddDayGoalMainView.swift
    
    deleted:
    AddDayGoalControllerMainView.swift
    
    renamed:
    DailyGoalTextFieldView.swift
    ReccomendDailyWaterView.swift

---
+++ Waterly App/Controllers/Main/AddDayGoalController/Assambly/AddDayGoalAssambly.swift
@@ -0,0 +1,8 @@
+//
+//  AddDayGoalAssambly.swift
+//  Waterly App
+//
+//  Created by Olegio on 21.01.2023.
+//
+
+import Foundation
---
+++ Waterly App/Controllers/Main/AddDayGoalController/Views/AddDayGoalMainView.swift
@@ -0,0 +1,147 @@
+//
+//  AddDayGoalControllerMainView.swift
+//  Waterly App
+//
+//  Created by Olegio on 20.01.2023.
+//
+
+import UIKit
+
+enum AddDayGoalMainViewElements {
+    case dismissButton
+    case titleLabel
+    case textFieldView
+    case recomendDailyWaterView
+    case saveButton
+}
+
+protocol AddDayGoalMainViewDelegate: AnyObject {
+    
+    func dismissButtonDidPressed()
+    func saveButtonDidPressed()
+}
+
+class AddDayGoalMainView: BaseView {
+    
+    weak var delegate: AddDayGoalMainViewDelegate?
+    
+    private let dismissButton = CustomButton(with: .text)
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.sfProSemibold(size: 16)
+        label.textColor = Resources.Colors.Text.textMain
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let textFieldView = DailyGoalTextFieldView()
+    
+    private let recomendDailyWaterView = ReccomendDailyWaterView()
+    
+    private let saveButton = CustomButton(with: .fill)
+    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+}
+
+extension AddDayGoalMainView {
+    
+    override func addViews() {
+        super.addViews()
+        
+        addView(dismissButton)
+        addView(titleLabel)
+        addView(textFieldView)
+        
+        addView(recomendDailyWaterView)
+        addView(saveButton)
+    }
+    
+    override func layoutViews() {
+        super.layoutViews()
+        
+        NSLayoutConstraint.activate([
+            dismissButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            dismissButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 30),
+            dismissButton.trailingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -5),
+            dismissButton.bottomAnchor.constraint(equalTo: textFieldView.topAnchor, constant: -32),
+            dismissButton.widthAnchor.constraint(equalToConstant: 80),
+            dismissButton.heightAnchor.constraint(equalToConstant: 25),
+            
+            titleLabel.topAnchor.constraint(equalTo: dismissButton.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: dismissButton.bottomAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            titleLabel.widthAnchor.constraint(equalToConstant: 180),
+            titleLabel.heightAnchor.constraint(equalToConstant: 25),
+            
+            textFieldView.leadingAnchor.constraint(equalTo: dismissButton.leadingAnchor),
+            textFieldView.topAnchor.constraint(equalTo: dismissButton.bottomAnchor, constant: 32),
+            textFieldView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            textFieldView.bottomAnchor.constraint(equalTo: recomendDailyWaterView.topAnchor, constant: -32),
+            
+            recomendDailyWaterView.leadingAnchor.constraint(equalTo: dismissButton.leadingAnchor),
+            recomendDailyWaterView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            recomendDailyWaterView.topAnchor.constraint(equalTo: textFieldView.bottomAnchor, constant: 32),
+            
+            saveButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            saveButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            saveButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -40)
+        ])
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+        
+//        addDayGoalViewModel = AddDayGoalViewModel()
+//
+//        textFieldView.dailyGoalViewModel = DailyGoalTextFieldViewModel(
+//            text: "\(addDayGoalViewModel.dailyGoal)",
+//            placeholder: addDayGoalViewModel.textFieldPlaceholder
+//        )
+
+        dismissButton.addTarget(self, action: #selector(dismissButtonAction), for: .touchUpInside)
+                        
+        saveButton.setColor(for: Resources.Colors.Accent.accentMain, title: .white)
+        saveButton.addTarget(self, action: #selector(saveButtonAction), for: .touchUpInside)
+    }
+}
+
+// Get Access to private values
+extension AddDayGoalMainView {
+    
+    func getTitleLabel() -> UILabel {
+        return titleLabel
+    }
+    
+    func getDismissButton() -> CustomButton {
+        return dismissButton
+    }
+    
+    func getSaveButton() -> CustomButton {
+        return saveButton
+    }
+    
+    func getTextFieldView() -> DailyGoalTextFieldView {
+        return textFieldView
+    }
+}
+
+@objc extension AddDayGoalMainView {
+    
+    func dismissButtonAction() {
+//        dismiss(animated: true)
+        delegate?.dismissButtonDidPressed()
+    }
+    
+    func saveButtonAction() {
+//        guard let newValue = Int(textFieldView.getCurrentTextFieldText()) else { return }
+//        addDayGoalViewModel.saveButtonPressed(newValue)
+        delegate?.saveButtonDidPressed()
+    }
+}
