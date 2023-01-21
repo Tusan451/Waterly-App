fe184dc18f8d8b05dbe0467c32e03b4bbbaa711c
Oleg Fedotov <trevor706@gmail.com>
2023-01-21 18:25:58 +0300

    Create Assambly layer for AddDayGoalModule

--- Waterly App/Controllers/Main/AddDayGoalController/Assambly/AddDayGoalAssambly.swift
+++ Waterly App/Controllers/Main/AddDayGoalController/Assambly/AddDayGoalAssambly.swift
@@ -5,4 +5,18 @@
 //  Created by Olegio on 21.01.2023.
 //
 
-import Foundation
+import UIKit
+
+class AddDayGoalAssambly {
+    
+    class func configuredModule(_ delegate: UIViewController) -> AddDayGoalViewController {
+        
+        let viewController = AddDayGoalViewController()
+        let viewModel = AddDayGoalViewModel()
+
+        viewController.delegate = delegate as? any ModalViewControllerDelegate
+        viewController.addDayGoalViewModel = viewModel
+
+        return viewController
+    }
+}
