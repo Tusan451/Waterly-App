180e6338ec3c6b062496d08f29dfe6341d46ad79
Oleg Fedotov <trevor706@gmail.com>
2023-01-24 19:44:36 +0300

    Modify Assambly.swift

--- Waterly App/Helpers/Assambly.swift
+++ Waterly App/Helpers/Assambly.swift
@@ -10,13 +10,18 @@ import UIKit
 class ModuleBuilder {
     
     class func configureAddDayGoalModule(_ delegate: UIViewController) -> UIViewController {
-        
         let viewController = AddDayGoalViewController()
         let viewModel = AddDayGoalViewModel()
-
         viewController.delegate = delegate as? any ModalViewControllerDelegate
         viewController.addDayGoalViewModel = viewModel
-
+        return viewController
+    }
+    
+    class func configureAddWaterModule(_ delegate: UIViewController) -> UIViewController {
+        let viewController = AddWaterViewController()
+        viewController.delegate = delegate as? any ModalViewControllerDelegate
+        let presenter = AddWaterPresenter(view: viewController)
+        viewController.presenter = presenter
         return viewController
     }
 }
