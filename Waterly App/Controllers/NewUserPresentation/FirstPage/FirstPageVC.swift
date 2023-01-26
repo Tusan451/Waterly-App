3d5d9a7791cab8500e248cb70b193899f9b52ff0
Oleg Fedotov <trevor706@gmail.com>
2023-01-26 19:50:21 +0300

    Add settings in FirstPageVC

--- Waterly App/Controllers/NewUserPresentation/FirstPage/FirstPageVC.swift
+++ Waterly App/Controllers/NewUserPresentation/FirstPage/FirstPageVC.swift
@@ -9,16 +9,44 @@ import UIKit
 
 final class FirstPageVC: BaseController {
     
-    
+    var mainView = FirstPageVCMainView()
+    var presenter: FirstPagePresenterProtocol!
 }
 
 extension FirstPageVC {
     
-    override func addViews() {
-        super.addViews()
+    override func loadView() {
+        view = mainView
     }
     
     override func configureViews() {
         super.configureViews()
+        
+        presenter.setIcon()
+        presenter.setMainTitle()
+        presenter.setSecondaryTitle()
+        presenter.setAuthorTitle()
+        
+        self.view.backgroundColor = Resources.Colors.Back.newUserPresentationBack
+    }
+}
+
+extension FirstPageVC: FirstPageViewProtocol {
+    
+    func getIcon(name: String) {
+        guard let iconImage = UIImage(named: name) else { return }
+        mainView.iconView.image = iconImage
+    }
+    
+    func getMainTitle(text: String) {
+        mainView.mainLabel.text = text
+    }
+    
+    func getSecondaryTitle(text: String) {
+        mainView.secondaryLabel.text = text
+    }
+    
+    func getAuthorTitle(text: String) {
+        mainView.authorLabel.text = text
     }
 }
