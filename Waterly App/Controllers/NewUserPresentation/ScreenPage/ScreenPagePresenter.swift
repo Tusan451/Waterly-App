8e6ff25149b720a76f3271796e7194e6ed67741f
Oleg Fedotov <trevor706@gmail.com>
2023-01-27 12:07:29 +0300

    Add ScreenPagePresenter.swift

--- Waterly App/Controllers/NewUserPresentation/ScreenPage/ScreenPagePresenter.swift
+++ Waterly App/Controllers/NewUserPresentation/ScreenPage/ScreenPagePresenter.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+protocol ScreenPageViewProtocol: AnyObject {
+    func setContent(title: String, imageName: String, message: String)
+}
+
+protocol ScreenPagePresenterProtocol: AnyObject {
+    var title: String { get }
+    var imageName: String { get }
+    var message: String { get }
+    init(view: ScreenPageViewProtocol, title: String, imageName: String, message: String)
+    func configureContent()
+}
+
+class ScreenPagePresenter: ScreenPagePresenterProtocol {
+    
+    weak var view: ScreenPageViewProtocol?
+    
+    var title: String
+    
+    var imageName: String
+    
+    var message: String
+    
+    required init(view: ScreenPageViewProtocol, title: String, imageName: String, message: String) {
+        self.view = view
+        self.title = title
+        self.imageName = imageName
+        self.message = message
+    }
+    
+    func configureContent() {
+        self.view?.setContent(title: self.title,
+                              imageName: self.imageName,
+                              message: self.message)
+    }
+}
