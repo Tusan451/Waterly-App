3609fce2ef138696d6670c322eec587e7f563b62
Oleg Fedotov <trevor706@gmail.com>
2023-01-27 12:08:21 +0300

    Add ScreenPageViewController.swift

--- Waterly App/Controllers/NewUserPresentation/ScreenPage/ScreenPageViewController.swift
+++ Waterly App/Controllers/NewUserPresentation/ScreenPage/ScreenPageViewController.swift
@@ -5,4 +5,35 @@
 //  Created by Olegio on 26.01.2023.
 //
 
-import Foundation
+import UIKit
+
+final class ScreenPageViewController: BaseController {
+    
+    var mainView = ScreenPageMainView()
+    var presenter: ScreenPagePresenterProtocol!
+}
+
+extension ScreenPageViewController {
+    
+    override func loadView() {
+        super.loadView()
+        
+        view = mainView
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+        
+        presenter.configureContent()
+        
+        self.view.backgroundColor = Resources.Colors.Back.newUserPresentationBack
+    }
+}
+
+extension ScreenPageViewController: ScreenPageViewProtocol {
+    func setContent(title: String, imageName: String, message: String) {
+        mainView.titleLabel.text = title
+        mainView.messageLabel.text = message
+        mainView.imageView.image = UIImage(named: imageName)
+    }
+}
