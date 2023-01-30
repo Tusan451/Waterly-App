85099a20b0050a00335450851fa11e621524e67a
Oleg Fedotov <trevor706@gmail.com>
2023-01-30 20:24:48 +0300

    Add mainView for Settings main screen module

--- Waterly App/Controllers/Settings/MainModule/SettingsMainView.swift
+++ Waterly App/Controllers/Settings/MainModule/SettingsMainView.swift
@@ -5,4 +5,108 @@
 //  Created by Olegio on 30.01.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol SettingsMainViewDelegate: AnyObject {
+    
+    func userSettingsMenuItemTapped()
+    func appSettingsMenuItemTapped()
+}
+
+final class SettingsMainView: BaseView {
+    
+    weak var delegate: SettingsMainViewDelegate?
+    
+    let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.sfProRegular(size: 20)
+        label.textColor = Resources.Colors.Text.textMain
+        return label
+    }()
+    
+    let userInfoLabel: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.sfProRegular(size: 14)
+        label.textColor = Resources.Colors.Text.textSecondary
+        return label
+    }()
+    
+    let userSettingsMenuItemButton = MenuItemButton(
+        title: Resources.Strings.SettingsModule.mainControllerUserSettingsTitle,
+        image: Resources.Images.SettingsModule.user)
+    
+    let appSettingsMenuItemButton = MenuItemButton(
+        title: Resources.Strings.SettingsModule.mainControllerAppSettingsTitle,
+        image: Resources.Images.SettingsModule.settings)
+    
+    private let menuItemsStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 16
+        return view
+    }()
+}
+
+extension SettingsMainView {
+    
+    override func addViews() {
+        super.addViews()
+        
+        addView(userNameLabel)
+        addView(userInfoLabel)
+        
+        menuItemsStackView.addArrangedSubview(userSettingsMenuItemButton)
+        menuItemsStackView.addArrangedSubview(appSettingsMenuItemButton)
+        
+        addView(menuItemsStackView)
+    }
+    
+    override func layoutViews() {
+        super.layoutViews()
+        
+        NSLayoutConstraint.activate([
+            userNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            userNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 24),
+            userNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            userNameLabel.bottomAnchor.constraint(equalTo: userInfoLabel.topAnchor, constant: -8),
+            
+            userInfoLabel.leadingAnchor.constraint(equalTo: userNameLabel.leadingAnchor),
+            userInfoLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8),
+            userInfoLabel.trailingAnchor.constraint(equalTo: userNameLabel.trailingAnchor),
+            userInfoLabel.bottomAnchor.constraint(equalTo: menuItemsStackView.topAnchor, constant: -32),
+
+            userSettingsMenuItemButton.heightAnchor.constraint(equalToConstant: 40),
+            appSettingsMenuItemButton.heightAnchor.constraint(equalToConstant: 40),
+
+            menuItemsStackView.leadingAnchor.constraint(equalTo: userNameLabel.leadingAnchor),
+            menuItemsStackView.topAnchor.constraint(equalTo: userInfoLabel.bottomAnchor, constant: 32),
+            menuItemsStackView.trailingAnchor.constraint(equalTo: userNameLabel.trailingAnchor)
+        ])
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+        
+        userNameLabel.text = "Олег Федотов"
+        userInfoLabel.text = "31 год, 74 кг, 178 см"
+        
+        userSettingsMenuItemButton.addTarget(self,
+                                             action: #selector(userSettingsMenuItemButtonAction),
+                                             for: .touchUpInside)
+        
+        appSettingsMenuItemButton.addTarget(self,
+                                             action: #selector(appSettingsMenuItemButtonAction),
+                                             for: .touchUpInside)
+    }
+}
+
+@objc extension SettingsMainView {
+    
+    func userSettingsMenuItemButtonAction() {
+        delegate?.userSettingsMenuItemTapped()
+    }
+    
+    func appSettingsMenuItemButtonAction() {
+        delegate?.appSettingsMenuItemTapped()
+    }
+}
