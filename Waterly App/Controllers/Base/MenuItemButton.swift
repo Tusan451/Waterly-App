d26917a95333b38c6a2a89936d4ae5dd9a494bd3
Oleg Fedotov <trevor706@gmail.com>
2023-01-30 19:51:24 +0300

    Add new files

---
+++ Waterly App/Controllers/Base/MenuItemButton.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItemButton.swift
+//  Waterly App
+//
+//  Created by Olegio on 30.01.2023.
+//
+
+import Foundation
---
+++ Waterly App/Controllers/Settings/MainModule/SettingsMainView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsMainView.swift
+//  Waterly App
+//
+//  Created by Olegio on 30.01.2023.
+//
+
+import Foundation
