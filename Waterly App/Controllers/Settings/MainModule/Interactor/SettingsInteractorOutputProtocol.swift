db36cd62a13fef8e35014a1af50679ed1e0708c3
Oleg Fedotov <trevor706@gmail.com>
2023-01-31 14:52:08 +0300

    Functionality description of SettingsInteractorOutputProtocol.swift

--- Waterly App/Controllers/Settings/MainModule/Interactor/SettingsInteractorOutputProtocol.swift
+++ Waterly App/Controllers/Settings/MainModule/Interactor/SettingsInteractorOutputProtocol.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+protocol SettingsInteractorOutputProtocol: AnyObject {
+    func receiveUserData(userData: UserData)
+    func receiveUserMenuItemData(menuItemData: MenuItemData)
+    func receiveAppMenuItemData(menuItemData: MenuItemData)
+    func receiveNavigationBarTitle(title: String)
+    func receiveTabBarTitle(title: String)
+    func receiveUserSettingsMenuItemAction(with message: String)
+    func receiveAppSettingsMenuItemAction(with message: String)
+}
