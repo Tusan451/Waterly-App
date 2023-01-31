4a8fc26bab62039dbbc573c73ac69a7f34429957
Oleg Fedotov <trevor706@gmail.com>
2023-01-31 16:00:18 +0300

    Description of functionality of SettingsViewInputProtocol.swift

--- Waterly App/Controllers/Settings/MainModule/View/SettingsViewInputProtocol.swift
+++ Waterly App/Controllers/Settings/MainModule/View/SettingsViewInputProtocol.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+protocol SettingsViewInputProtocol: AnyObject {
+    func setUserName(_ userName: String)
+    func setUserInfo(_ userInfo: String)
+    func setUserSettingsMenuItem(title: String, imageName: String)
+    func setAppSettingsMenuItem(title: String, imageName: String)
+    func setNavigationBarTitle(navBarTitle: String)
+    func setTabBarTitle(title: String)
+    func performUserSettingsMenuItemAction(with message: String)
+    func performAppSettingsMenuItemAction(with message: String)
+}
