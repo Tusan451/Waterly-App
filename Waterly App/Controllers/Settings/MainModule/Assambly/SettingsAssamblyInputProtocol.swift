179f51d122c2f5e80d7caf836f3bac812a94bc9a
Oleg Fedotov <trevor706@gmail.com>
2023-01-31 14:49:20 +0300

    Implement logic of assambly settings module

--- Waterly App/Controllers/Settings/MainModule/Assambly/SettingsAssamblyInputProtocol.swift
+++ Waterly App/Controllers/Settings/MainModule/Assambly/SettingsAssamblyInputProtocol.swift
@@ -5,4 +5,22 @@
 //  Created by Olegio on 31.01.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol SettingsAssamblyInputProtocol {
+    func configure() -> UIViewController
+}
+
+class SettingsModuleAssambly: SettingsAssamblyInputProtocol {
+    
+    func configure() -> UIViewController {
+        let viewController = SettingsViewController()
+        let presenter = SettingsPresenter(view: viewController)
+        let interactor = SettingsInteractor(presenter: presenter)
+        
+        viewController.presenter = presenter
+        presenter.interactor = interactor
+        
+        return viewController
+    }
+}
