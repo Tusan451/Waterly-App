622310feb18bf8d1569c3d7420ac33b8fef3d747
Oleg Fedotov <trevor706@gmail.com>
2023-01-31 14:50:02 +0300

    Implement settings interactor

--- Waterly App/Controllers/Settings/MainModule/Interactor/SettingsInteractor.swift
+++ Waterly App/Controllers/Settings/MainModule/Interactor/SettingsInteractor.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+class SettingsInteractor: SettingsInteractorInputProtocol {
+    
+    unowned let presenter: SettingsInteractorOutputProtocol
+    
+    required init(presenter: SettingsInteractorOutputProtocol) {
+        self.presenter = presenter
+    }
+    
+    func provideUserData() {
+        let userData = UserData(userName: "Олег Федотов",
+                                userAge: "31",
+                                userWeight: "76",
+                                userHeight: "178")
+        
+        presenter.receiveUserData(userData: userData)
+    }
+    
+    func provideUserMenuItemData() {
+        let userMenuItemData = MenuItemData(
+            title: Resources.Strings.SettingsModule.mainControllerUserSettingsTitle,
+            imageName: Resources.Images.SettingsModule.user
+        )
+        presenter.receiveUserMenuItemData(menuItemData: userMenuItemData)
+    }
+    
+    func provideAppMenuItemData() {
+        let appMenuItemData = MenuItemData(
+            title: Resources.Strings.SettingsModule.mainControllerAppSettingsTitle,
+            imageName: Resources.Images.SettingsModule.settings
+        )
+        presenter.receiveAppMenuItemData(menuItemData: appMenuItemData)
+    }
+    
+    func provideNavigationBarTitle() {
+        let title = Resources.Strings.SettingsModule.mainControllerTitle
+        presenter.receiveNavigationBarTitle(title: title)
+    }
+    
+    func provideTabBarTitle() {
+        let title = Resources.Strings.SettingsModule.mainControllerTitle
+        presenter.receiveTabBarTitle(title: title)
+    }
+    
+    func performUserSettingsMenuItemAction() {
+        let message = "userSettingsMenuItemTapped"
+        presenter.receiveUserSettingsMenuItemAction(with: message)
+    }
+    
+    func performAppSettingsMenuItemAction() {
+        let message = "appSettingsMenuItemTapped"
+        presenter.receiveAppSettingsMenuItemAction(with: message)
+    }
+}
