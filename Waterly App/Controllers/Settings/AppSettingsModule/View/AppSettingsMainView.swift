9a271e16cc261f6b1fe17234cae9c81eb7b9487b
Oleg Fedotov <trevor706@gmail.com>
2023-02-01 12:30:20 +0300

    Add new files

---
+++ Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsMainView.swift
@@ -0,0 +1,8 @@
+//
+//  AppSettingsMainView.swift
+//  Waterly App
+//
+//  Created by Olegio on 31.01.2023.
+//
+
+import Foundation
---
+++ Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AppSettingsViewController.swift
+//  Waterly App
+//
+//  Created by Olegio on 31.01.2023.
+//
+
+import Foundation
