2d2ce54a314291845b17b11fc6ed9a4ba5c4cd82
Oleg Fedotov <trevor706@gmail.com>
2023-02-01 12:33:31 +0300

    Add main view in AppSettingsViewController.swift

--- Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsViewController.swift
+++ Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsViewController.swift
@@ -5,4 +5,51 @@
 //  Created by Olegio on 31.01.2023.
 //
 
-import Foundation
+import UIKit
+
+final class AppSettingsViewController: BaseController {
+    
+    var mainView = AppSettingsMainView()
+}
+
+extension AppSettingsViewController {
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        title = "Приложение"
+    }
+}
+
+// MARK: - AppSettingsMainViewDelegate
+
+extension AppSettingsViewController: AppSettingsMainViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainView.delegate = self
+    }
+    
+    func notificationsMenuItemSwitch(param: UISwitch) {
+        param.isOn ? print("notifications isOn") : print("notifications isOff")
+    }
+    
+    func lightThemeMenuItemSwitch(param: UISwitch) {
+        param.isOn ? print("lightTheme isOn") : print("lightTheme isOff")
+    }
+    
+    func darkThemeMenuItemSwitch(param: UISwitch) {
+        param.isOn ? print("darkTheme isOn") : print("darkTheme isOff")
+    }
+    
+    func systemThemeMenuItemSwitch(param: UISwitch) {
+        param.isOn ? print("systemTheme isOn") : print("systemTheme isOff")
+    }
+}
