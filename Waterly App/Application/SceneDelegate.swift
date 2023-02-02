277a472da115748241c37179fd79189e56b66429
Oleg Fedotov <trevor706@gmail.com>
2023-02-02 14:23:00 +0300

    Modify SceneDelegate.swift
    add setTheme func for window

--- Waterly App/Application/SceneDelegate.swift
+++ Waterly App/Application/SceneDelegate.swift
@@ -9,20 +9,48 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
-    var window: UIWindow?
+    var window: UIWindow? {
+        didSet {
+            window?.overrideUserInterfaceStyle = setTheme()
+        }
+    }
     
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
         
         let tabBarController = TabBarController()
-//        let newUserQuestionaryController = ModuleBuilder.configureNewUserFinalPageModule()
         
         guard let windowScene = scene as? UIWindowScene else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
+    }
+}
+
+// MARK: - Private extension
+
+private extension SceneDelegate {
+    
+    func setTheme() -> UIUserInterfaceStyle {
+        
+        let currentTheme = DataManager.shared.getTheme()
+        
+        var appTheme: UIUserInterfaceStyle = .unspecified
+        
+        switch currentTheme {
+        case 0:
+            appTheme = .unspecified
+        case 1:
+            appTheme = .light
+        case 2:
+            appTheme = .dark
+        default:
+            break
+        }
+        
+        return appTheme
     }
 }
 
