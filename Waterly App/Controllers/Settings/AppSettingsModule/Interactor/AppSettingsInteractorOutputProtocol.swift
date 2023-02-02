36c0d0bf9746f462cedef14c11e925eddb80e62b
Oleg Fedotov <trevor706@gmail.com>
2023-02-02 14:11:55 +0300

    Add AppSettingsInteractorOutputProtocol.swift

--- Waterly App/Controllers/Settings/AppSettingsModule/Interactor/AppSettingsInteractorOutputProtocol.swift
+++ Waterly App/Controllers/Settings/AppSettingsModule/Interactor/AppSettingsInteractorOutputProtocol.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol AppSettingsInteractorOutputProtocol: AnyObject {
+    func receiveMainTitle(_ title: String)
+    func receiveNotificationsMenuItem(_ item: MenuItem)
+    func receiveLightThemeMenuItem(_ item: MenuItem)
+    func receiveDarkThemeMenuItem(_ item: MenuItem)
+    func receiveSystemThemeMenuItem(_ item: MenuItem)
+    func receiveUpdatedMenuItemsStatus(lightThemeIsOn: Bool,
+                                       darkThemeIsOn: Bool,
+                                       systemThemeIsOn: Bool)
+    func receiveCurrentAppTheme(_ theme: Int)
+    func receiveNotificationsStatus(_ isOn: Bool)
+}
