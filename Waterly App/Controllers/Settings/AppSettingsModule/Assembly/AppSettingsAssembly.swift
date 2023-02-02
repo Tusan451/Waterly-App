62b9dd8111a52abd7a9794a62b2ea32e6b28f82f
Oleg Fedotov <trevor706@gmail.com>
2023-02-02 14:09:25 +0300

    Add AppSettingsAssembly.swift

--- Waterly App/Controllers/Settings/AppSettingsModule/Assembly/AppSettingsAssembly.swift
+++ Waterly App/Controllers/Settings/AppSettingsModule/Assembly/AppSettingsAssembly.swift
@@ -5,4 +5,22 @@
 //  Created by Olegio on 01.02.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol AppSettingsAssemblyInputProtocol {
+    func configure() -> UIViewController
+}
+
+class AppSettingsModuleAssambly: AppSettingsAssemblyInputProtocol {
+    
+    func configure() -> UIViewController {
+        let viewController = AppSettingsViewController()
+        let presenter = AppSettingsPresenter(viewController)
+        let interactor = AppSettingsInteractor(presenter)
+        
+        viewController.presenter = presenter
+        presenter.interactor = interactor
+        
+        return viewController
+    }
+}
