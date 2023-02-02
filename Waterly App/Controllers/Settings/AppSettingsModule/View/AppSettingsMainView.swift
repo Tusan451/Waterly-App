9acd60bf5ef7aa496b43d48e56e06422cf0856a2
Oleg Fedotov <trevor706@gmail.com>
2023-02-02 14:13:49 +0300

    Modify AppSettingsMainView.swift

--- Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsMainView.swift
+++ Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsMainView.swift
@@ -58,28 +58,25 @@ extension AppSettingsMainView {
     override func configureViews() {
         super.configureViews()
         
-        notificationsMenuItemView.setMainTitle(
-            Resources.Strings.SettingsModule.AppSettingsModule.notificationsMainTitle
-        )
-        notificationsMenuItemView.setSecondaryTitle(
-            Resources.Strings.SettingsModule.AppSettingsModule.notificationsSecondaryTitle
-        )
-        
-        lightThemeMenuItemView.setMainTitle(
-            Resources.Strings.SettingsModule.AppSettingsModule.lightThemeMainTitle
-        )
-        
-        darkThemeMenuItemView.setMainTitle(
-            Resources.Strings.SettingsModule.AppSettingsModule.darkThemeMainTitle
-        )
-        
-        systemThemeMenuItemView.setMainTitle(
-            Resources.Strings.SettingsModule.AppSettingsModule.systemThemeMainTitle
-        )
-        
-        notificationsMenuItemView.menuItemSwitch.setOn(true, animated: true)
-        lightThemeMenuItemView.menuItemSwitch.setOn(true, animated: true)
-        
+//        notificationsMenuItemView.setMainTitle(
+//            Resources.Strings.SettingsModule.AppSettingsModule.notificationsMainTitle
+//        )
+//        notificationsMenuItemView.setSecondaryTitle(
+//            Resources.Strings.SettingsModule.AppSettingsModule.notificationsSecondaryTitle
+//        )
+//        
+//        lightThemeMenuItemView.setMainTitle(
+//            Resources.Strings.SettingsModule.AppSettingsModule.lightThemeMainTitle
+//        )
+//        
+//        darkThemeMenuItemView.setMainTitle(
+//            Resources.Strings.SettingsModule.AppSettingsModule.darkThemeMainTitle
+//        )
+//        
+//        systemThemeMenuItemView.setMainTitle(
+//            Resources.Strings.SettingsModule.AppSettingsModule.systemThemeMainTitle
+//        )
+                
         notificationsMenuItemView.menuItemSwitch.addTarget(
             self,
             action: #selector(notificationsMenuItemAction),
