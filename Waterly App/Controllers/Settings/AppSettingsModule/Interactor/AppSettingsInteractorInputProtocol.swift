7b65e4ce89575f051f6a17638cf5ca0fb0682d73
Oleg Fedotov <trevor706@gmail.com>
2023-02-02 14:11:20 +0300

    Add AppSettingsInteractorInputProtocol.swift

--- Waterly App/Controllers/Settings/AppSettingsModule/Interactor/AppSettingsInteractorInputProtocol.swift
+++ Waterly App/Controllers/Settings/AppSettingsModule/Interactor/AppSettingsInteractorInputProtocol.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+protocol AppSettingsInteractorInputProtocol {
+    var notificationsIsOn: Bool { get set }
+    var lightThemeIsOn: Bool { get set }
+    var darkThemeIsOn: Bool { get set }
+    var systemThemeIsOn: Bool { get set }
+    init(_ presenter: AppSettingsInteractorOutputProtocol)
+    func provideMainTitle()
+    func provideNotificationsMenuItem()
+    func provideLightThemeMenuItem()
+    func provideDarkThemeMenuItem()
+    func provideSystemThemeMenuItem()
+    func provideCurrentAppTheme()
+    func toggleLightThemeStatus()
+    func toggleDarkThemeStatus()
+    func toggleSystemThemeStatus()
+    func toggleNotificationsStatus()
+}
