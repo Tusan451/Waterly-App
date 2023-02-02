0501f52e4ae99735b027eba3e40d9e259e4ef5e1
Oleg Fedotov <trevor706@gmail.com>
2023-02-02 14:16:57 +0300

    Add AppSettingsViewOutputProtocol

--- Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsViewOutputProtocol.swift
+++ Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsViewOutputProtocol.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+protocol AppSettingsViewOutputProtocol {
+    init(_ view: AppSettingsViewInputProtocol)
+    func provideMainTitle()
+    func provideNotificationsMenuItem()
+    func provideLightThemeMenuItem()
+    func provideDarkThemeMenuItem()
+    func provideSystemThemeMenuItem()
+    func provideCurrentAppTheme()
+    func lightMenuItemSwitchToggle()
+    func darkMenuItemSwitchToggle()
+    func systemMenuItemSwitchToggle()
+    func notificationsMenuItemSwitchToggle()
+}
