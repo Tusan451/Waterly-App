6bc73447c873a2758d659c73f4a1041057def10c
Oleg Fedotov <trevor706@gmail.com>
2023-02-02 14:21:38 +0300

    Add SettingsRouter.swift

--- Waterly App/Controllers/Settings/MainModule/Router/SettingsRouter.swift
+++ Waterly App/Controllers/Settings/MainModule/Router/SettingsRouter.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol SettingsRouterInputProtocol {
+    init(_ viewController: SettingsViewController)
+    func goToAppSettingsModule()
+}
+
+class SettingsRouter: SettingsRouterInputProtocol {
+    
+    unowned let viewController: SettingsViewController
+    
+    required init(_ viewController: SettingsViewController) {
+        self.viewController = viewController
+    }
+    
+    func goToAppSettingsModule() {
+        let appSettingsAssemblyModule: AppSettingsAssemblyInputProtocol = AppSettingsModuleAssambly()
+        let appSettingsVc = appSettingsAssemblyModule.configure()
+        viewController.navigationController?.pushViewController(appSettingsVc, animated: true)
+    }
+}
