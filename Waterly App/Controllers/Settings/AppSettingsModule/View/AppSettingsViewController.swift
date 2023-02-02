5e1f07ac654edae038dc861ceab894b5666e7561
Oleg Fedotov <trevor706@gmail.com>
2023-02-02 14:15:22 +0300

    Modify AppSettingsViewController.swift
    add presenter
    add AppSettingsViewInputProtocol implementation

--- Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsViewController.swift
+++ Waterly App/Controllers/Settings/AppSettingsModule/View/AppSettingsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class AppSettingsViewController: BaseController {
     
     var mainView = AppSettingsMainView()
+    
+    var presenter: AppSettingsViewOutputProtocol!
 }
 
 extension AppSettingsViewController {
@@ -23,7 +25,12 @@ extension AppSettingsViewController {
         
         navigationItem.largeTitleDisplayMode = .never
         
-        title = "Приложение"
+        presenter.provideMainTitle()
+        presenter.provideNotificationsMenuItem()
+        presenter.provideLightThemeMenuItem()
+        presenter.provideDarkThemeMenuItem()
+        presenter.provideSystemThemeMenuItem()
+        presenter.provideCurrentAppTheme()
     }
 }
 
@@ -38,18 +45,98 @@ extension AppSettingsViewController: AppSettingsMainViewDelegate {
     }
     
     func notificationsMenuItemSwitch(param: UISwitch) {
-        param.isOn ? print("notifications isOn") : print("notifications isOff")
+        presenter.notificationsMenuItemSwitchToggle()
     }
     
     func lightThemeMenuItemSwitch(param: UISwitch) {
-        param.isOn ? print("lightTheme isOn") : print("lightTheme isOff")
+        presenter.lightMenuItemSwitchToggle()
     }
     
     func darkThemeMenuItemSwitch(param: UISwitch) {
-        param.isOn ? print("darkTheme isOn") : print("darkTheme isOff")
+        presenter.darkMenuItemSwitchToggle()
     }
     
     func systemThemeMenuItemSwitch(param: UISwitch) {
-        param.isOn ? print("systemTheme isOn") : print("systemTheme isOff")
+        presenter.systemMenuItemSwitchToggle()
+    }
+}
+
+// MARK: - AppSettingsViewInputProtocol
+
+extension AppSettingsViewController: AppSettingsViewInputProtocol {
+    
+    func setMainTitle(_ title: String) {
+        self.title = title
+    }
+    
+    func setNotificationsMenuItem(_ item: MenuItem) {
+        mainView.notificationsMenuItemView.setMainTitle(item.mainTitle)
+        mainView.notificationsMenuItemView.setSecondaryTitle(item.secondaryTitle)
+        mainView.notificationsMenuItemView.menuItemSwitch.setOn(item.isOn, animated: true)
+    }
+    
+    func setLightThemeMenuItem(_ item: MenuItem) {
+        mainView.lightThemeMenuItemView.setMainTitle(item.mainTitle)
+        mainView.lightThemeMenuItemView.setSecondaryTitle(item.secondaryTitle)
+        mainView.lightThemeMenuItemView.menuItemSwitch.setOn(item.isOn, animated: true)
+    }
+    
+    func setDarkThemeMenuItem(_ item: MenuItem) {
+        mainView.darkThemeMenuItemView.setMainTitle(item.mainTitle)
+        mainView.darkThemeMenuItemView.setSecondaryTitle(item.secondaryTitle)
+        mainView.darkThemeMenuItemView.menuItemSwitch.setOn(item.isOn, animated: true)
+    }
+    
+    func setSystemThemeMenuItem(_ item: MenuItem) {
+        mainView.systemThemeMenuItemView.setMainTitle(item.mainTitle)
+        mainView.systemThemeMenuItemView.setSecondaryTitle(item.secondaryTitle)
+        mainView.systemThemeMenuItemView.menuItemSwitch.setOn(item.isOn, animated: true)
+    }
+    
+    func setUpdatedSwitchesValues(
+        lightThemeIsOn: Bool,
+        darkThemeIsOn: Bool,
+        systemThemeIsOn: Bool
+    ) {
+        mainView.lightThemeMenuItemView.menuItemSwitch.setOn(lightThemeIsOn, animated: true)
+        mainView.darkThemeMenuItemView.menuItemSwitch.setOn(darkThemeIsOn, animated: true)
+        mainView.systemThemeMenuItemView.menuItemSwitch.setOn(systemThemeIsOn, animated: true)
+    }
+    
+    func setCurrentAppTheme(_ theme: Int) {
+        setTheme(theme)
+    }
+    
+    func setNotificationsSwitchValue(_ isOn: Bool) {
+        mainView.notificationsMenuItemView.menuItemSwitch.setOn(isOn, animated: true)
+    }
+}
+
+// MARK: - Private extension
+
+private extension AppSettingsViewController {
+    
+    func setTheme(_ theme: Int) {
+        
+        let keyWindow = UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .compactMap({$0 as? UIWindowScene})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first
+        
+        var appTheme: UIUserInterfaceStyle = .unspecified
+        
+        switch theme {
+        case 0:
+            appTheme = .unspecified
+        case 1:
+            appTheme = .light
+        case 2:
+            appTheme = .dark
+        default:
+            break
+        }
+        
+        keyWindow?.overrideUserInterfaceStyle = appTheme
     }
 }
