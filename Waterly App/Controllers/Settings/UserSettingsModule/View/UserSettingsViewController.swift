f4ec80795a4c752ad2c0350765902a7d45a6a53e
Oleg Fedotov <trevor706@gmail.com>
2023-02-07 13:50:04 +0300

    Modify UserSettingsModule/View/UserSettingsViewController.swift

--- Waterly App/Controllers/Settings/UserSettingsModule/View/UserSettingsViewController.swift
+++ Waterly App/Controllers/Settings/UserSettingsModule/View/UserSettingsViewController.swift
@@ -5,4 +5,58 @@
 //  Created by Olegio on 02.02.2023.
 //
 
-import Foundation
+import UIKit
+
+final class UserSettingsViewController: BaseController {
+    
+    var mainView = UserSettingsMainView()
+}
+
+extension UserSettingsViewController {
+    
+    override func loadView() {
+        super.loadView()
+        
+        view = mainView
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+        navigationItem.largeTitleDisplayMode = .never
+        title = Resources.Strings.SettingsModule.mainControllerUserSettingsTitle
+    }
+}
+
+extension UserSettingsViewController: UserSettingsMainViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainView.delegate = self
+    }
+    
+    func maleRadioButtonDidPressed() {
+        print("maleRadioButtonDidPressed")
+    }
+    
+    func femaleRadioButtonDidPressed() {
+        print("femaleRadioButtonDidPressed")
+    }
+    
+    func sliderChanged() {
+        print(mainView.physicalActivityView.physicalActivitySlider.value)
+    }
+    
+    func saveButtonAction() {
+        print("saveButtonDidPressed")
+    }
+}
+
+extension UserSettingsViewController: UITextFieldDelegate {
+    
+    // Hide keybord on tap on the screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+}
