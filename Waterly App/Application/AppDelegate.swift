20ada39c423e6030a8681d4c01e8d7048ffe2326
Oleg Fedotov <trevor706@gmail.com>
2023-02-07 13:50:47 +0300

    Modify AppDelegate.swift
    add notifications

--- Waterly App/Application/AppDelegate.swift
+++ Waterly App/Application/AppDelegate.swift
@@ -6,12 +6,66 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    let notificationCenter = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        requestAuthorization()
         return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+    }
+    
+    func requestAuthorization() {
+        
+        notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            
+            print("Permission granted: \(granted)")
+            
+            guard granted else { return }
+            
+            self.getNotificationSettings()
+        }
+    }
+    
+    func getNotificationSettings() {
+        
+        notificationCenter.getNotificationSettings { settings in
+            
+            print("Notification settings: \(settings)")
+        }
+    }
+    
+    func scheduleNotification(notificationType: String) {
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = notificationType
+        content.body = "Добавьте воду, чтобы следить за тем, сколько выпили сегодня"
+        content.sound = UNNotificationSound.default
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5,
+                                                        repeats: false)
+        
+        let identifier = "Local notification"
+        
+        let request = UNNotificationRequest(identifier: identifier,
+                                            content: content,
+                                            trigger: trigger)
+        
+        notificationCenter.add(request) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
