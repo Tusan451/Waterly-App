1241998844dd1f6cfb5093834dcc390c9d199858
Oleg Fedotov <trevor706@gmail.com>
2023-02-07 13:16:14 +0300

    Modify TabBarController.swift
    switch to .settings

--- Waterly App/Controllers/Base/TabBarController.swift
+++ Waterly App/Controllers/Base/TabBarController.swift
@@ -21,7 +21,7 @@ final class TabBarController: UITabBarController {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
         configureTabs()
-        switchTo(tab: .main)
+        switchTo(tab: .settings)
     }
     
     required init?(coder: NSCoder) {
