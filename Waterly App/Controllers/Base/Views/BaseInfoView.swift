ee94b8cd55caff9fb1137520e8820a43ce853493
Oleg Fedotov <trevor706@gmail.com>
2023-02-11 11:42:06 +0300

    Modify BaseInfoView.swift
    remove init
    modify setActivityColor method

--- Waterly App/Controllers/Base/Views/BaseInfoView.swift
+++ Waterly App/Controllers/Base/Views/BaseInfoView.swift
@@ -15,7 +15,7 @@ enum ActivityType: String {
 
 class BaseInfoView: BaseView {
     
-    private let titleLabel: UILabel = {
+    let titleLabel: UILabel = {
         let label = UILabel()
         label.font = Resources.Fonts.sfProRegular(size: 13)
         label.textColor = Resources.Colors.Text.textSecondary
@@ -36,18 +36,21 @@ class BaseInfoView: BaseView {
         return view
     }()
     
-    private let iconView = UIImageView()
+    let iconView = UIImageView()
     
-    init(with title: String, iconImage: UIImage? = nil) {
-        titleLabel.text = title
-        iconView.image = iconImage?.withRenderingMode(.alwaysTemplate)
-        
-        super.init(frame: .zero)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+//    init(with title: String, iconImage: UIImage? = nil) {
+//        super.init(frame: .zero)
+//        titleLabel.text = title
+//        iconView.image = iconImage?.withRenderingMode(.alwaysTemplate)
+//    }
+//
+//    init() {
+//        super.init(frame: .zero)
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
     
     // TODO: - Изменить функцию установки значения в зависимости от данных из БД
     func setValueLabel(with value: String) {
@@ -58,12 +61,12 @@ class BaseInfoView: BaseView {
     func setActivityColor(for type: ActivityType) {
         switch type {
         case .low:
-            iconView.tintColor = Resources.Colors.Other.yellow
-            valueLabel.textColor = Resources.Colors.Other.yellow
-            valueLabel.text = type.rawValue
-        case .medium:
             iconView.tintColor = Resources.Colors.Other.green
             valueLabel.textColor = Resources.Colors.Other.green
+            valueLabel.text = type.rawValue
+        case .medium:
+            iconView.tintColor = Resources.Colors.Other.yellow
+            valueLabel.textColor = Resources.Colors.Other.yellow
             valueLabel.text = type.rawValue
         case .high:
             iconView.tintColor = Resources.Colors.Other.red
