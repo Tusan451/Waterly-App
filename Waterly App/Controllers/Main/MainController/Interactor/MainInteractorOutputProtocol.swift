3d3963288f45266872f509f85546b17cb8a48af3
Oleg Fedotov <trevor706@gmail.com>
2023-02-11 11:45:45 +0300

    Modify MainInteractorOutputProtocol.swift

--- Waterly App/Controllers/Main/MainController/Interactor/MainInteractorOutputProtocol.swift
+++ Waterly App/Controllers/Main/MainController/Interactor/MainInteractorOutputProtocol.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+protocol MainInteractorOutputProtocol: AnyObject {
+    func receiveMainTitle(title: String)
+    func receiveUserName(name: String)
+    func receiveDailyWaterValues(title: String, image: String, value: Int)
+    func receiveActivityValues(title: String, image: String, activityType: ActivityType)
+}
