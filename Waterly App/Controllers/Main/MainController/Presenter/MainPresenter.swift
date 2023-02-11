1fd8dd635ea1f284c8afa1a9dbd376f3679ea87e
Oleg Fedotov <trevor706@gmail.com>
2023-02-11 11:46:22 +0300

    Modify MainPresenter.swift

--- Waterly App/Controllers/Main/MainController/Presenter/MainPresenter.swift
+++ Waterly App/Controllers/Main/MainController/Presenter/MainPresenter.swift
@@ -1,5 +1,5 @@
 //
-//  MainViewPresenter.swift
+//  MainPresenter.swift
 //  Waterly App
 //
 //  Created by Olegio on 09.02.2023.
@@ -7,6 +7,42 @@
 
 import Foundation
 
-class MainViewPresenter {
+class MainPresenter: MainViewOutputProtocol {
+    unowned let view: MainViewInputProtocol
+    var interactor: MainInteractorInputProtocol!
     
+    required init(_ view: MainViewInputProtocol) {
+        self.view = view
+    }
+    
+    func provideNavigationBarLabels() {
+        interactor.provideNavigationBarLabels()
+    }
+    
+    func provideDashboardInfoValues() {
+        interactor.provideDashboardInfoValues()
+    }
+}
+
+// MARK: - MainInteractorOutputProtocol
+
+extension MainPresenter: MainInteractorOutputProtocol {
+    
+    func receiveMainTitle(title: String) {
+        view.setMainTitle(title: title)
+    }
+    
+    func receiveUserName(name: String) {
+        let navigationTitle = "Привет, \(name)!"
+        view.setNavigationBarTitle(navBarTitle: navigationTitle)
+    }
+    
+    func receiveDailyWaterValues(title: String, image: String, value: Int) {
+        let waterRate = "\(value) мл"
+        view.setDailyWaterView(title: title, image: image, value: waterRate)
+    }
+    
+    func receiveActivityValues(title: String, image: String, activityType: ActivityType) {
+        view.setActivityView(title: title, image: image, activityType: activityType)
+    }
 }
