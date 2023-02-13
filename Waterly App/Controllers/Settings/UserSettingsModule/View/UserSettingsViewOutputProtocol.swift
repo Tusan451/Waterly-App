37e495823ca352ac480bc97bd062b90b6b452ce9
Oleg Fedotov <trevor706@gmail.com>
2023-02-13 18:54:01 +0300

    Modify UserSettingsViewOutputProtocol.swift

--- Waterly App/Controllers/Settings/UserSettingsModule/View/UserSettingsViewOutputProtocol.swift
+++ Waterly App/Controllers/Settings/UserSettingsModule/View/UserSettingsViewOutputProtocol.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+protocol UserSettingsViewOutputProtocol {
+    init(_ view: UserSettingsViewInputProtocol)
+    func provideDefaultLabelsSettings()
+    func sliderValueDidChanged(to value: Float)
+    func provideSaveButtonStateWith(nameValue: String,
+                                    ageValue: String,
+                                    heightValue: String,
+                                    weightValue: String)
+    func maleRButtonDidPressed()
+    func femaleRButtonDidPressed()
+    func saveButtonDidPressed(name: String,
+                              male: RadioButtonState,
+                              female: RadioButtonState,
+                              age: String,
+                              height: String,
+                              weight: String,
+                              activity: Float)
+}
