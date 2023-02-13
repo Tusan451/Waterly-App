1b1f95ffbf92aa73170470e9cd26da863a8fcd9d
Oleg Fedotov <trevor706@gmail.com>
2023-02-13 18:44:53 +0300

    Modify UserSettingsInteractorInputProtocol.swift

--- Waterly App/Controllers/Settings/UserSettingsModule/Interactor/UserSettingsInteractorInputProtocol.swift
+++ Waterly App/Controllers/Settings/UserSettingsModule/Interactor/UserSettingsInteractorInputProtocol.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol UserSettingsInteractorInputProtocol {
+    init(_ presenter: UserSettingsInteractotOutputProtocol)
+    func provideDefaultValues()
+    func provideActivityLabel(for sliderValue: Float)
+    func provideSaveButtonStateWith(_ currentInputValues: CurrentInputValues)
+    func maleRButtonDidPressed()
+    func femaleRButtonDidPressed()
+    func saveButtonDidPressed(name: String,
+                              male: RadioButtonState,
+                              female: RadioButtonState,
+                              age: String,
+                              height: String,
+                              weight: String,
+                              activity: Float)
+}
