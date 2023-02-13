306a59b39d5983c324a7afe91ff79c6db3423b15
Oleg Fedotov <trevor706@gmail.com>
2023-02-13 18:43:32 +0300

    Modify UserSettingsAssembley.swift

--- Waterly App/Controllers/Settings/UserSettingsModule/Assembley/UserSettingsAssembley.swift
+++ Waterly App/Controllers/Settings/UserSettingsModule/Assembley/UserSettingsAssembley.swift
@@ -5,4 +5,24 @@
 //  Created by Olegio on 07.02.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol UserSettingsAssembleyInputProtocol {
+    func configure() -> UIViewController
+}
+
+class UserSettingsAssembley: UserSettingsAssembleyInputProtocol {
+    
+    func configure() -> UIViewController {
+        let viewController = UserSettingsViewController()
+        let presenter = UserSettingsPresenter(viewController)
+        let interactor = UserSettingsInteractor(presenter)
+        let router = UserSettingsRouter(viewController)
+        
+        viewController.presenter = presenter
+        presenter.interactor = interactor
+        presenter.router = router
+        
+        return viewController
+    }
+}
