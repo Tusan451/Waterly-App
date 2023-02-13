d7b9f82403ffaf98a9348dd48a0177f9eeff00bd
Oleg Fedotov <trevor706@gmail.com>
2023-02-13 18:46:32 +0300

    Modify UserSettingsRouter.swift

--- Waterly App/Controllers/Settings/UserSettingsModule/Router/UserSettingsRouter.swift
+++ Waterly App/Controllers/Settings/UserSettingsModule/Router/UserSettingsRouter.swift
@@ -1,5 +1,5 @@
 //
-//  UserSettingsRouterInputProtocol.swift
+//  UserSettingsRouter.swift
 //  Waterly App
 //
 //  Created by Olegio on 08.02.2023.
@@ -9,5 +9,27 @@ import UIKit
 
 protocol UserSettingsRouterInputProtocol {
     init(_ viewController: UIViewController)
-    func showAlert()
+    func showAlert(title: String, message: String?, actionTitle: String)
+}
+
+class UserSettingsRouter: UserSettingsRouterInputProtocol {
+    unowned let viewController: UIViewController
+    
+    required init(_ viewController: UIViewController) {
+        self.viewController = viewController
+    }
+    
+    func showAlert(title: String, message: String?, actionTitle: String) {
+        let viewController = self.viewController as! UserSettingsViewController
+        let targetTab: Tabs = .main
+        let alert = ConfigAlertService.shared.configureAlert(
+            title: title,
+            message: message,
+            actionTitle: actionTitle) { _ in
+                viewController.tabBarController?.selectedIndex = targetTab.rawValue
+                viewController.navigationController?.popViewController(animated: false)
+            }
+        
+        viewController.present(alert, animated: true)
+    }
 }
