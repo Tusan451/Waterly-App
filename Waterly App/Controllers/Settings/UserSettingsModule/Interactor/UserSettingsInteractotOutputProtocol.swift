1f41fde9f0b09b978bacd53bb275df6885841a6c
Oleg Fedotov <trevor706@gmail.com>
2023-02-13 18:45:24 +0300

    Modify UserSettingsInteractotOutputProtocol.swift

--- Waterly App/Controllers/Settings/UserSettingsModule/Interactor/UserSettingsInteractotOutputProtocol.swift
+++ Waterly App/Controllers/Settings/UserSettingsModule/Interactor/UserSettingsInteractotOutputProtocol.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol UserSettingsInteractotOutputProtocol: AnyObject {
+    func receiveDefaultValues(_ defaultValues: DefaultValues)
+    func receiveNameTextFieldValues(_ title: String, _ value: String?)
+    func receiveAgeTextFieldValues(_ title: String, _ value: String?, _ placeholder: String)
+    func receiveHeightTextFieldValues(_ title: String, _ value: String?, _ placeholder: String)
+    func receiveWeightTextFieldValues(_ title: String, _ value: String?, _ placeholder: String)
+    func receiveUpdatedActivityLabel(_ title: String, _ color: SliderColor)
+    func receiveSaveButtonState(with state: ButtonState)
+    func receiveRButtonsStates(maleRButtonState: RadioButtonState,
+                               femaleRButtonState: RadioButtonState)
+    func userDataDidSaved()
+}
