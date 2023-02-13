d1ea51482b65511174feb8e9d6f6a27be9710ebe
Oleg Fedotov <trevor706@gmail.com>
2023-02-13 18:56:34 +0300

    Modify ConfigAlertService.swift

--- Waterly App/Services/ConfigAlertService.swift
+++ Waterly App/Services/ConfigAlertService.swift
@@ -5,4 +5,27 @@
 //  Created by Olegio on 08.02.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol ConfigAlertServiceProtocol {
+    func configureAlert(title: String,
+                        message: String?,
+                        actionTitle: String,
+                        completion: @escaping (UIAlertAction) -> Void) -> UIAlertController
+}
+
+class ConfigAlertService: ConfigAlertServiceProtocol {
+    static let shared = ConfigAlertService()
+    
+    func configureAlert(title: String,
+                        message: String?,
+                        actionTitle: String,
+                        completion: @escaping (UIAlertAction) -> Void) -> UIAlertController {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: actionTitle, style: .default, handler: completion)
+        alertController.addAction(action)
+        
+        return alertController
+    }
+}
