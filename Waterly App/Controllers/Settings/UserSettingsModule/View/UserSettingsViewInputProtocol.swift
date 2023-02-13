92f99f6f6abb2ea488e7879e108fa0d61db02d3f
Oleg Fedotov <trevor706@gmail.com>
2023-02-13 18:52:26 +0300

    Modify UserSettingsViewInputProtocol.swift

--- Waterly App/Controllers/Settings/UserSettingsModule/View/UserSettingsViewInputProtocol.swift
+++ Waterly App/Controllers/Settings/UserSettingsModule/View/UserSettingsViewInputProtocol.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol UserSettingsViewInputProtocol: AnyObject {
+    func setScreenTitle(_ title: String)
+    func configureNameTextField(_ title: String, _ value: String?)
+    func configureAgeTextField(_ title: String, _ value: String?, _ placeholder: String)
+    func configureHeightTextField(_ title: String, _ value: String?, _ placeholder: String)
+    func configureWeightTextField(_ title: String, _ value: String?, _ placeholder: String)
+    func setGenderTitle(_ title: String)
+    func setMaleTitle(_ title: String)
+    func setFemaleTitle(_ title: String)
+    func setActivityTitle(_ title: String)
+    func setButtonTitle(_ title: String)
+    func setDisclaimer(_ disclaimer: String)
+    func setUpdatedActivityLabel(with title: String, and color: SliderColor)
+    func setSaveButtonState(with state: ButtonState)
+    func setRButtonsStates(male: RadioButtonState, female: RadioButtonState)
+}
