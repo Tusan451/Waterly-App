7c953ba53d2243d26c087e30895525a21f9d86eb
Oleg Fedotov <trevor706@gmail.com>
2023-02-19 22:14:49 +0300

    Modify MainViewInputProtocol.swift

--- Waterly App/Controllers/Main/MainController/Views/MainViewInputProtocol.swift
+++ Waterly App/Controllers/Main/MainController/Views/MainViewInputProtocol.swift
@@ -12,4 +12,9 @@ protocol MainViewInputProtocol: AnyObject {
     func setMainTitle(title: String)
     func setDailyWaterView(title: String, image: String, value: String)
     func setActivityView(title: String, image: String, activityType: ActivityType)
+    func setGoalViewDefaults(title: String, image: String)
+    func setWaterGoalValue(value: String)
+    func setWaterProgress(progress: Double, goal: Double, percent: CGFloat)
+    func setWaterHistoryTitle(title: String)
+    func setWaterCapacity(capacity: [WaterCapacity])
 }
