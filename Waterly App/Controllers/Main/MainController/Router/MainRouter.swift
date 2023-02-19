ad4727c5ea31ca0d13ae506237f7d9787733dab1
Oleg Fedotov <trevor706@gmail.com>
2023-02-19 22:10:58 +0300

    Modify MainRouter.swift

--- Waterly App/Controllers/Main/MainController/Router/MainRouter.swift
+++ Waterly App/Controllers/Main/MainController/Router/MainRouter.swift
@@ -5,4 +5,30 @@
 //  Created by Olegio on 15.02.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol MainRouterInputProtocol {
+    init(_ viewController: UIViewController)
+    func goToAddDayGoalModule()
+    func goToAddWaterModule()
+}
+
+class MainRouter: MainRouterInputProtocol {
+    unowned let viewController: UIViewController
+    
+    required init(_ viewController: UIViewController) {
+        self.viewController = viewController
+    }
+    
+    func goToAddDayGoalModule() {
+        let viewController = self.viewController as! MainViewController
+        let addDayGoalVc = ModuleBuilder.configureAddDayGoalModule(viewController)
+        viewController.present(addDayGoalVc, animated: true)
+    }
+    
+    func goToAddWaterModule() {
+        let viewController = self.viewController as! MainViewController
+        let addWaterVc = ModuleBuilder.configureAddWaterModule(viewController)
+        viewController.present(addWaterVc, animated: true)
+    }
+}
