e869700113d016559b1a055f21e845325026753f
Oleg Fedotov <trevor706@gmail.com>
2023-02-19 22:18:39 +0300

    Modify UserDataManager.swift

--- Waterly App/Services/UserDataManager.swift
+++ Waterly App/Services/UserDataManager.swift
@@ -10,12 +10,16 @@ import Foundation
 private let dataKey = "userData"
 
 protocol UserDataManagerProtocol {
-    init(encoder: JSONEncoder, decoder: JSONDecoder)
+    init(userDefaults: UserDefaults, encoder: JSONEncoder, decoder: JSONDecoder)
     func saveUserData(_ userData: UserDataModel)
     func getUserData(completion: @escaping (Result<UserDataModel, Error>) -> Void)
     func removeUserData()
     func saveWaterRate(_ value: Int?, for userID: UUID)
     func getWaterRate(for key: String?) -> Int?
+    func saveWaterGoal(_ value: Int?, for key: String)
+    func getWaterGoal(for key: String?) -> Int?
+    func saveWaterProgress(_ value: Int?, for key: String)
+    func getWaterProgress(for key: String?) -> Int?
 }
 
 enum DataManagerError: Error {
@@ -23,16 +27,22 @@ enum DataManagerError: Error {
 }
 
 class UserDataManager: UserDataManagerProtocol {
-    private let userDefaults = UserDefaults.standard
+    
     static let shared = UserDataManager()
+    private let userDefaults: UserDefaults
     private let encoder: JSONEncoder
     private let decoder: JSONDecoder
-    private var user: UserDataModel? = nil
+//    private var user: UserDataModel? = nil
     
-    required init(encoder: JSONEncoder = .init(), decoder: JSONDecoder = .init()) {
+    required init(userDefaults: UserDefaults = .standard,
+                  encoder: JSONEncoder = .init(),
+                  decoder: JSONDecoder = .init()) {
+        self.userDefaults = userDefaults
         self.encoder = encoder
         self.decoder = decoder
     }
+    
+    // MARK: - UserData
     
     func saveUserData(_ userData: UserDataModel) {
         let queue = DispatchQueue.global(qos: .utility)
@@ -53,7 +63,6 @@ class UserDataManager: UserDataManagerProtocol {
         }
         do {
             let userPossibleData = try self.decoder.decode(UserDataModel.self, from: data)
-            print("PossibleUserDATA: \(userPossibleData)")
             completion(.success(userPossibleData))
         } catch {
             completion(.failure(error))
@@ -65,11 +74,37 @@ class UserDataManager: UserDataManagerProtocol {
         userDefaults.removeObject(forKey: dataKey)
     }
     
+    // MARK: - WaterRate
+    
     func saveWaterRate(_ value: Int?, for userID: UUID) {
         userDefaults.set(value, forKey: "\(userID)")
     }
     
     func getWaterRate(for key: String?) -> Int? {
+        guard let key = key else { return nil }
+        return userDefaults.object(forKey: key) == nil ?
+        nil : userDefaults.integer(forKey: key)
+    }
+    
+    // MARK: - WaterGoal
+    
+    func saveWaterGoal(_ value: Int?, for key: String) {
+        userDefaults.set(value, forKey: key)
+    }
+    
+    func getWaterGoal(for key: String?) -> Int? {
+        guard let key = key else { return nil }
+        return userDefaults.object(forKey: key) == nil ?
+        nil : userDefaults.integer(forKey: key)
+    }
+    
+    // MARK: - WaterProgress
+    
+    func saveWaterProgress(_ value: Int?, for key: String) {
+        userDefaults.set(value, forKey: key)
+    }
+    
+    func getWaterProgress(for key: String?) -> Int? {
         guard let key = key else { return nil }
         return userDefaults.object(forKey: key) == nil ?
         nil : userDefaults.integer(forKey: key)
